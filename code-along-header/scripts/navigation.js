48ba77a205f5826168f902caf9ae070d5cc5d011
charsle3 <jpkingthe3rd@gmail.com>
2026-03-06 09:02:20 -0800

    code-along

---
+++ code-along-header/scripts/navigation.js
@@ -0,0 +1,8 @@
+const navButton = document.querySelector("#ham-btn");
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+const navBar = document.querySelector("#nav-bar");
